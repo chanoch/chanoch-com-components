df4bd7bf0f01c150d0d0687c2cd907621693c8fe
Chanoch <chanoch@clearbrook.it>
2018-04-02 22:34:28 +0100

    Initial release.
    Move components from chanoch.com project.
    Expose as Ccc export.

---
+++ index.js
@@ -0,0 +1,27 @@
+import CallToAction from './components/CallToAction/CallToAction';
+import Declaration from './components/Declaration/Declaration';
+import Divider from './components/Divider/Divider';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import ImageLink from './components/ImageLink/ImageLink';
+import Navigation from './components/Navigation/Navigation';
+import PhoneLink from './components/PhoneLink/PhoneLink';
+import Quote from './components/Quote/Quote';
+import ReservationForm from './components/ReservationForm/ReservationForm';
+import Signature from './components/Signature/Signature';
+
+var Ccc = {
+    CallToAction,
+    Declaration,
+    Divider,
+    Footer,
+    Header,
+    ImageLink,
+    Navigation,
+    PhoneLink,
+    Quote,
+    ReservationForm,
+    Signature
+}
+
+module.exports = Ccc;
